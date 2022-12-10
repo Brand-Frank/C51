3660e77d89457d1ceaa75eabe1bccac5cfb1860a
Brand-Frank <3069584010@qq.com>
2022-12-10 21:05:46 +0800

    BUG:一开始指定D1的状态后，按下开关不反应

--- 3.2 独立按键控制LED状态/main.c
+++ 3.2 独立按键控制LED状态/main.c
@@ -5,14 +5,16 @@
 //#define TWO
 //#define THREE
 
-sbit LED_1 = P2^0;
-sbit LED_2 = P2^1;
-sbit LED_3 = P2^2;
-sbit LED_4 = P2^3;
-sbit LED_5 = P2^4;
-sbit LED_6 = P2^5;
-sbit LED_7 = P2^6;
-sbit LED_8 = P2^7;
+sbit D1 = P2^0;
+sbit D2 = P2^1;
+sbit D3 = P2^2;
+sbit D4 = P2^3;
+sbit D5 = P2^4;
+sbit D6 = P2^5;
+sbit D7 = P2^6;
+sbit D8 = P2^7;
+
+sbit K1 = P3^1;
 
 void Delay(unsigned int ms);
 //void Delay500ms();
@@ -22,14 +24,14 @@ void main(void)
 {
 	while(1)
 	{
-		LED_1 = 0;
-		LED_2 = 1;
-		//Delay500ms();
-		Delay(50000);
-		LED_1 = 1;
-		LED_2 = 0;
-		//Delay500ms();
-		Delay(50000);
+		D1 = 0;	//一开始灯是亮的，按一次，取一次反
+		if(K1 == 0)
+		{
+			Delay(20);	//按下消抖(Press to remove shaking)
+			while(K1 == 0);	//检测按键是否松开，没松开就一直停在此处，否则松开了，执行后面的语句(Check whether the key is released. If not, stop here all the time. If not, execute the following statement)
+			Delay(20);	//松开消抖(Release to eliminate shaking)
+			D1 = ~D1;
+		}
 	}
 }
 
@@ -45,31 +47,16 @@ void Delay(unsigned int ms)		//@11.0592MHz
 	unsigned char i, j;
 
 	_nop_();
-	i = 2;
-	j = 199;
+	
 	do
 	{
+		i = 2;
+		j = 199;
 		do
 		{
-			while(ms--);
-		}
-		while (--j);
-	} while (--i);
+			
+			while (--j);
+		} while (--i);
+	}while(ms--);
+	
 }
-
-//void Delay500ms()		//@11.0592MHz
-//{
-//	unsigned char i, j, k;
-
-//	_nop_();
-//	i = 4;
-//	j = 129;
-//	k = 119;
-//	do
-//	{
-//		do
-//		{
-//			while (--k);
-//		} while (--j);
-//	} while (--i);
-//}
