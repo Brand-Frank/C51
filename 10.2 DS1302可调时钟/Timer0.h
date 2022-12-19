f4aff0ae9baea518146714392b4cce304327a60a
Brand-Frank <3069584010@qq.com>
2022-12-19 22:37:42 +0800

    Finish the clock(可调时钟)

--- 10.2 DS1302可调时钟/Timer0.h
+++ 10.2 DS1302可调时钟/Timer0.h
@@ -1,6 +1,6 @@
 #ifndef __TIMER0_H__
 #define __TIMER0_H__
 
-void Timer0_Init();
+void Timer0_Init(void);
 
 #endif // __TIMER0_H__
--- 10.2 DS1302可调时钟/main.c
+++ 10.2 DS1302可调时钟/main.c
@@ -5,7 +5,7 @@
 #include "key.h"
 #include "Timer0.h"
 
-unsigned char KeyNum, MODE, TimeSetSelect;
+unsigned char KeyNum, MODE, TimeSetSelect, TimeSetFlashFlag;//时间设置闪烁标志位
 //按下按键1，改变MODE
 void TimeShow(void)
 {
@@ -18,7 +18,7 @@ void TimeShow(void)
 	LCD_ShowNum(2, 7, DS1302_Time[5], 2);	
 }
 
-//按键2，设置年月日时分秒
+//按键2，设置年月日时分秒并存储在DS1302_Time[]数组中
 void TimeSet(void)
 {
 	if(KeyNum == 2)	//Feature2: 切换 年/月/日/时/分/秒 6种单独的模式，初始化按2时没用
@@ -95,20 +95,29 @@ void TimeSet(void)
 		if(DS1302_Time[4] < 0)	{ DS1302_Time[4] = 59; }	//从0回到59分
 		if(DS1302_Time[5] < 0)	{ DS1302_Time[5] = 59; }	//从0回到59秒
 	}
+	// TimeSetFlashFlag每隔0.5s取一次反，下方条件也是间隔0.5s闪烁一次
+	if(TimeSetSelect == 0 && TimeSetFlashFlag == 1) { LCD_ShowString(1, 1, "  ");	}
+	else {	LCD_ShowNum(1, 1, DS1302_Time[0], 2);	}
+	if(TimeSetSelect == 1 && TimeSetFlashFlag == 1) { LCD_ShowString(1, 4, "  ");	}
+	else {	LCD_ShowNum(1, 4, DS1302_Time[1], 2);	}
+	if(TimeSetSelect == 2 && TimeSetFlashFlag == 1) { LCD_ShowString(1, 7, "  ");	}
+	else {	LCD_ShowNum(1, 7, DS1302_Time[2], 2);	}
+	if(TimeSetSelect == 3 && TimeSetFlashFlag == 1) { LCD_ShowString(2, 1, "  ");	}
+	else {	LCD_ShowNum(2, 1, DS1302_Time[3], 2);	}
+	if(TimeSetSelect == 4 && TimeSetFlashFlag == 1) { LCD_ShowString(2, 4, "  ");	}
+	else {	LCD_ShowNum(2, 4, DS1302_Time[4], 2);	}
+	if(TimeSetSelect == 5 && TimeSetFlashFlag == 1) { LCD_ShowString(2, 7, "  ");	}
+	else {	LCD_ShowNum(2, 7, DS1302_Time[5], 2);	}
 	
-	LCD_ShowNum(1, 1, DS1302_Time[0], 2);
-	LCD_ShowNum(1, 4, DS1302_Time[1], 2);
-	LCD_ShowNum(1, 7, DS1302_Time[2], 2);
-	LCD_ShowNum(2, 1, DS1302_Time[3], 2);
-	LCD_ShowNum(2, 4, DS1302_Time[4], 2);
-	LCD_ShowNum(2, 7, DS1302_Time[5], 2);	
-	LCD_ShowNum(2, 10, TimeSetSelect, 2);	//TEST: 测试切换的按键
+	//LCD_ShowNum(2, 10, TimeSetSelect, 2);	//TEST: 测试切换的按键
+	//LCD_ShowNum(2, 13, TimeSetFlashFlag, 1);	//TEST: 测试时间设置闪烁标志位
 }
 
 void main(void)
 {
 	LCD_Init();
 	DS1302_Init();
+	Timer0_Init();
 	LCD_ShowString(1, 1, "  -  -  ");
 	LCD_ShowString(2, 1, "  :  :  ");
 	
@@ -119,13 +128,34 @@ void main(void)
 		KeyNum = key();
 		if(KeyNum == 1)	//Feature1: 按键1连按交替改变按键模式
 		{
-			if(MODE == 0)	{	MODE = 1;	}
-			else if(MODE == 1)	{	MODE = 0;	}
+			if(MODE == 0)
+			{
+				MODE = 1;
+				TimeSetSelect = 0;/*每次从年开始设置*/
+			}
+			else if(MODE == 1)
+			{
+				MODE = 0;
+				DS1302_SetTime();
+			}	//Feature4: 切换回模式0显示时间时，从数组中读取当前数组中的时间
 		}
 		switch(MODE)	//交替调用两个函数:初始值为0，显示时间，当按下按键时，模式改为1，时间停止，可以设置时间
 		{
 			case 0:	TimeShow(); break;
 			case 1:	TimeSet();	break;
 		}
+	}
+}
+
+void Timer0_Routine(void) interrupt 1	//定时器/计数器初始化函数
+{
+	static unsigned int cnt;
+	TL0 = 0x66;		//设置定时初值
+	TH0 = 0xFC;		//设置定时初值
+	cnt++;
+	if(cnt >= 500)
+	{
+		cnt = 0;
+		TimeSetFlashFlag = !TimeSetFlashFlag;	//0/1
 	}
 }
